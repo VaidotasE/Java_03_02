237a8b299d28d7ec37154c033f96b86febb42282
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-31 11:42:06 +0200

    Day 3 code 02

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ivesk zodi:");
+
+        String zodis = "";
+        while (!zodis.equals("pabaiga")) {
+            zodis = sc.next();
+            System.out.println(zodis);
+        }
+
+    }
+
+}
